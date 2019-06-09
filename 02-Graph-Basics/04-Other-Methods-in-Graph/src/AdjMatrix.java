6721f7d74910f99d8b293e35572b5b9963945922
Yubo Liu <liuyubobobo@gmail.com>
2019-06-08 17:24:25 -0700

    section 02 chapter 04 codes added.

--- 02-Graph-Basics/04-Other-Methods-in-Graph/src/AdjMatrix.java
+++ 02-Graph-Basics/04-Other-Methods-in-Graph/src/AdjMatrix.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AdjMatrix {
@@ -14,12 +15,17 @@ public class AdjMatrix {
         try(Scanner scanner = new Scanner(file)){
 
             V = scanner.nextInt();
+            if(V < 0) throw new IllegalArgumentException("V must be non-negative");
             adj = new int[V][V];
 
             E = scanner.nextInt();
+            if(E < 0) throw new IllegalArgumentException("E must be non-negative");
+
             for(int i = 0; i < E; i ++){
                 int a = scanner.nextInt();
+                validateVertex(a);
                 int b = scanner.nextInt();
+                validateVertex(b);
                 adj[a][b] = 1;
                 adj[b][a] = 1;
             }
@@ -27,6 +33,45 @@ public class AdjMatrix {
         catch(IOException e){
             e.printStackTrace();
         }
+    }
+
+    private void validateVertex(int v){
+        if(v < 0 || v >= V)
+            throw new IllegalArgumentException("vertex " + v + "is invalid");
+    }
+
+    public int V(){
+        return V;
+    }
+
+    public int E(){
+        return E;
+    }
+
+    public int degree(int v){
+//        validateVertex(v);
+//        int res = 0;
+//        for(int i = 0; i < V; i ++)
+//            if(adj[v][i] == 1)
+//                res ++;
+//        return res;
+
+        return adj(v).size();
+    }
+
+    public ArrayList<Integer> adj(int v){
+        validateVertex(v);
+        ArrayList<Integer> res = new ArrayList<>();
+        for(int i = 0; i < V; i ++)
+            if(adj[v][i] == 1)
+                res.add(i);
+        return res;
+    }
+
+    public boolean isAdj(int v, int w){
+        validateVertex(v);
+        validateVertex(w);
+        return adj[v][w] == 1;
     }
 
     @Override
