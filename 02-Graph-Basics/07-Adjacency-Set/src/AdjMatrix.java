9b3af878111daad77facb01c47d9f2677a4a6c6f
Yubo Liu <liuyubobobo@gmail.com>
2019-06-09 16:03:34 -0700

    Chapter 02 section 03 added.

---
+++ 02-Graph-Basics/07-Adjacency-Set/src/AdjMatrix.java
@@ -0,0 +1,95 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AdjMatrix {
+
+    private int V;
+    private int E;
+    private int[][] adj;
+
+    public AdjMatrix(String pathStr){
+
+        File file = new File(pathStr);
+
+        try(Scanner scanner = new Scanner(file)){
+
+            V = scanner.nextInt();
+            if(V < 0) throw new IllegalArgumentException("V must be non-negative");
+            adj = new int[V][V];
+
+            E = scanner.nextInt();
+            if(E < 0) throw new IllegalArgumentException("E must be non-negative");
+
+            for(int i = 0; i < E; i ++){
+                int a = scanner.nextInt();
+                validateVertex(a);
+                int b = scanner.nextInt();
+                validateVertex(b);
+                adj[a][b] = 1;
+                adj[b][a] = 1;
+            }
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    private void validateVertex(int v){
+        if(v < 0 || v >= V)
+            throw new IllegalArgumentException("vertex " + v + "is invalid");
+    }
+
+    public int V(){
+        return V;
+    }
+
+    public int E(){
+        return E;
+    }
+
+    public int degree(int v){
+//        validateVertex(v);
+//        int res = 0;
+//        for(int i = 0; i < V; i ++)
+//            if(adj[v][i] == 1)
+//                res ++;
+//        return res;
+
+        return adj(v).size();
+    }
+
+    public ArrayList<Integer> adj(int v){
+        validateVertex(v);
+        ArrayList<Integer> res = new ArrayList<>();
+        for(int i = 0; i < V; i ++)
+            if(adj[v][i] == 1)
+                res.add(i);
+        return res;
+    }
+
+    public boolean isAdj(int v, int w){
+        validateVertex(v);
+        validateVertex(w);
+        return adj[v][w] == 1;
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(String.format("V = %d, E = %d\n", V, E));
+        for(int i = 0; i < V; i ++){
+            for(int j = 0; j < V; j ++)
+                sb.append(adj[i][j] + " ");
+            sb.append('\n');
+        }
+        return sb.toString();
+    }
+
+    public static void main(String args[]){
+
+        AdjMatrix adjMatrix = new AdjMatrix("g.txt");
+        System.out.print(adjMatrix);
+    }
+}
