658c4859b3f945fc0d070d2fa2d40651548f194c
Yubo Liu <liuyubobobo@gmail.com>
2019-07-06 16:41:00 -0700

    Chapter 04 CC codes added.

---
+++ 04-Graph-DFS-Application/01-Connected-Components-Count/src/CC.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+public class CC {
+
+    private Graph G;
+    private boolean[] visited;
+    private int cccount = 0;
+
+    public CC(Graph G){
+
+        this.G = G;
+        visited = new boolean[G.V()];
+        for(int v = 0; v < G.V(); v ++)
+            if(!visited[v]){
+                dfs(v);
+                cccount ++;
+            }
+    }
+
+    private void dfs(int v){
+
+        visited[v] = true;
+        for(int w: G.adj(v))
+            if(!visited[w])
+                dfs(w);
+    }
+
+    public int count(){
+        return cccount;
+    }
+
+    public static void main(String[] args){
+
+        Graph g = new Graph("g.txt");
+        CC cc = new CC(g);
+        System.out.println(cc.count());
+    }
+}
