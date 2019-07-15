aec68eedd38c3a0556ec22c46ed711c8200ed3df
Yubo Liu <liuyubobobo@gmail.com>
2019-07-14 18:34:10 -0700

    Chapter 04 section 04 completed.

---
+++ 04-Graph-DFS-Application/03-More-about-Connected-Components/src/CC.java
@@ -0,0 +1,69 @@
+import java.util.ArrayList;
+
+public class CC {
+
+    private Graph G;
+    private int[] visited;
+    private int cccount = 0;
+
+    public CC(Graph G){
+
+        this.G = G;
+        visited = new int[G.V()];
+        for(int i = 0; i < visited.length; i ++)
+            visited[i] = -1;
+
+        for(int v = 0; v < G.V(); v ++)
+            if(visited[v] == -1){
+                dfs(v, cccount);
+                cccount ++;
+            }
+    }
+
+    private void dfs(int v, int ccid){
+
+        visited[v] = ccid;
+        for(int w: G.adj(v))
+            if(visited[w] == -1)
+                dfs(w, ccid);
+    }
+
+    public int count(){
+        return cccount;
+    }
+
+    public boolean isConnected(int v, int w){
+        G.validateVertex(v);
+        G.validateVertex(w);
+        return visited[v] == visited[w];
+    }
+
+    public ArrayList<Integer>[] components(){
+
+        ArrayList<Integer>[] res = new ArrayList[cccount];
+        for(int i = 0; i < cccount; i ++)
+            res[i] = new ArrayList<Integer>();
+
+        for(int v = 0; v < G.V(); v ++)
+            res[visited[v]].add(v);
+        return res;
+    }
+
+    public static void main(String[] args){
+
+        Graph g = new Graph("g.txt");
+        CC cc = new CC(g);
+        System.out.println(cc.count());
+
+        System.out.println(cc.isConnected(0, 6));
+        System.out.println(cc.isConnected(5, 6));
+
+        ArrayList<Integer>[] comp = cc.components();
+        for(int ccid = 0; ccid < comp.length; ccid ++){
+            System.out.print(ccid + " : ");
+            for(int w: comp[ccid])
+                System.out.print(w + " ");
+            System.out.println();
+        }
+    }
+}
