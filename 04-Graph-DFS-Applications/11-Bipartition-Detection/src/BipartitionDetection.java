2281243c1c3a839679fddd252ed7715d88ba40e8
Yubo Liu <liuyubobobo@gmail.com>
2019-07-24 17:29:23 -0700

    Chapter 04 section 11 bug fixed.

--- 04-Graph-DFS-Applications/11-Bipartition-Detection/src/BipartitionDetection.java
+++ 04-Graph-DFS-Applications/11-Bipartition-Detection/src/BipartitionDetection.java
@@ -30,7 +30,7 @@ public class BipartitionDetection {
         colors[v] = color;
         for(int w: G.adj(v))
             if(!visited[w]){
-                if(dfs(w, 1 - color)) return false;
+                if(!dfs(w, 1 - color)) return false;
             }
             else if(colors[w] == colors[v])
                 return false;
@@ -46,10 +46,16 @@ public class BipartitionDetection {
         Graph g = new Graph("g.txt");
         BipartitionDetection bipartitionDetection = new BipartitionDetection(g);
         System.out.println(bipartitionDetection.isBipartite);
+        // true
 
         Graph g2 = new Graph("g2.txt");
         BipartitionDetection bipartitionDetection2 = new BipartitionDetection(g2);
         System.out.println(bipartitionDetection2.isBipartite);
+        // false
 
+        Graph g3 = new Graph("g3.txt");
+        BipartitionDetection bipartitionDetection3 = new BipartitionDetection(g3);
+        System.out.println(bipartitionDetection3.isBipartite);
+        // true
     }
 }
