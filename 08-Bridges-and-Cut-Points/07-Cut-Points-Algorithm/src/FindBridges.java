2797dad70e808e0bc3a2a5c2a7cf2200e896bf9b
Yubo Liu <liuyubobobo@gmail.com>
2019-08-16 11:31:33 -0700

    Chapter 08 codes completed.

---
+++ 08-Bridges-and-Cut-Points/07-Cut-Points-Algorithm/src/FindBridges.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+
+public class FindBridges {
+
+    private Graph G;
+    private boolean[] visited;
+
+    private int ord[];
+    private int low[];
+    private int cnt;
+
+    private ArrayList<Edge> res;
+
+    public FindBridges(Graph G){
+
+        this.G = G;
+        visited = new boolean[G.V()];
+
+        res = new ArrayList<>();
+        ord = new int[G.V()];
+        low = new int[G.V()];
+        cnt = 0;
+
+        for(int v = 0; v < G.V(); v ++)
+            if(!visited[v])
+                dfs(v, v);
+    }
+
+    private void dfs(int v, int parent){
+
+        visited[v] = true;
+        ord[v] = cnt;
+        low[v] = ord[v];
+        cnt ++;
+
+        for(int w: G.adj(v))
+            if(!visited[w]){
+                dfs(w, v);
+                low[v] = Math.min(low[v], low[w]);
+                if(low[w] > ord[v])
+                    res.add(new Edge(v, w));
+            }
+            else if(w != parent)
+                low[v] = Math.min(low[v], ord[w]);
+    }
+
+    public ArrayList<Edge> result(){
+        return res;
+    }
+
+    public static void main(String[] args){
+
+        Graph g = new Graph("g.txt");
+        FindBridges fb = new FindBridges(g);
+        System.out.println("Bridges in g : " + fb.result());
+
+        Graph g2 = new Graph("g2.txt");
+        FindBridges fb2 = new FindBridges(g2);
+        System.out.println("Bridges in g2 : " + fb2.result());
+
+        Graph tree = new Graph("tree.txt");
+        FindBridges fb3 = new FindBridges(tree);
+        System.out.println("Bridges in tree : " + fb3.result());
+    }
+}
---
+++ 08-Bridges-and-Cut-Points/07-Cut-Points-Algorithm/src/FindCutPoints.java
@@ -0,0 +1,74 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class FindCutPoints {
+
+    private Graph G;
+    private boolean[] visited;
+
+    private int ord[];
+    private int low[];
+    private int cnt;
+
+    private HashSet<Integer> res;
+
+    public FindCutPoints(Graph G){
+
+        this.G = G;
+        visited = new boolean[G.V()];
+
+        res = new HashSet<>();
+        ord = new int[G.V()];
+        low = new int[G.V()];
+        cnt = 0;
+
+        for(int v = 0; v < G.V(); v ++)
+            if(!visited[v])
+                dfs(v, v);
+    }
+
+    private void dfs(int v, int parent){
+
+        visited[v] = true;
+        ord[v] = cnt;
+        low[v] = ord[v];
+        cnt ++;
+
+        int child = 0;
+
+        for(int w: G.adj(v))
+            if(!visited[w]){
+                dfs(w, v);
+                low[v] = Math.min(low[v], low[w]);
+
+                child ++;
+                if(v == parent && child > 1)
+                    res.add(v);
+
+                if(v != parent && low[w] >= ord[v])
+                    res.add(v);
+            }
+            else if(w != parent)
+                low[v] = Math.min(low[v], ord[w]);
+    }
+
+    public HashSet<Integer> result(){
+        return res;
+    }
+
+    public static void main(String[] args){
+
+        Graph g = new Graph("g.txt");
+        FindCutPoints fc = new FindCutPoints(g);
+        System.out.println("Cut Points in g : " + fc.result());
+
+        Graph g2 = new Graph("g2.txt");
+        FindCutPoints fc2 = new FindCutPoints(g2);
+        System.out.println("Cut Points in g2 : " + fc2.result());
+
+        Graph tree = new Graph("tree.txt");
+        FindCutPoints fc3 = new FindCutPoints(tree);
+        System.out.println("Cut Points in tree : " + fc3.result());
+    }
+}
